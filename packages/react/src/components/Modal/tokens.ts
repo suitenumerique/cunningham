b04fdd32c7f599e6e47c39bcf5a84a40c5fa3708
Nathan Panchout <nathanpanchout@hotmail.com>
2025-08-22 10:15:51 +0200

    🎨(react) enhance Modal component styles
    update Modal token and css files with new css variables introduce
    with the new tokens architectures

--- packages/react/src/components/Modal/tokens.ts
+++ packages/react/src/components/Modal/tokens.ts
@@ -2,17 +2,17 @@ import { DefaultTokens } from "@openfun/cunningham-tokens";
 
 export const tokens = (defaults: DefaultTokens) => {
   return {
-    "background-color": defaults.theme.colors["greyscale-000"],
-    "backdrop-color": "#0C1A2B99",
+    "background-color": defaults.contextuals.background.surface.secondary,
+    "backdrop-color": "#00000099",
     "border-radius": "4px",
-    "border-color": defaults.theme.colors["greyscale-300"],
+    "border-color": defaults.contextuals.border.surface.primary,
     "box-shadow": "0px 1px 2px 0px #0C1A2B4D",
-    color: defaults.theme.colors["greyscale-900"],
+    color: defaults.contextuals.content.semantic.neutral.primary,
     // "backdrop-color": "#0C1A2B99", // Does not work yet due to backdrop CSS var support.
-    "title-font-weight": defaults.theme.font.weights.bold,
-    "content-font-size": defaults.theme.font.sizes.m,
-    "content-font-weight": defaults.theme.font.weights.regular,
-    "content-color": defaults.theme.colors["greyscale-800"],
+    "title-font-weight": defaults.globals.font.weights.bold,
+    "content-font-size": defaults.globals.font.sizes.sm,
+    "content-font-weight": defaults.globals.font.weights.regular,
+    "content-color": defaults.contextuals.content.semantic.neutral.primary,
     "width-small": "300px",
     "width-medium": "600px",
     "width-large": "800px",
