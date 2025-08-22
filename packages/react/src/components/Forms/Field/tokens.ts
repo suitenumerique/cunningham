db93b6f9feb747286958da3ca32bf400b3f8d3e7
Nathan Panchout <nathanpanchout@hotmail.com>
2025-08-22 10:12:54 +0200

    🎨(react) enhance field component styles
    update field token and css files with new css variables introduce
    with the new tokens architectures

--- packages/react/src/components/Forms/Field/tokens.ts
+++ packages/react/src/components/Forms/Field/tokens.ts
@@ -2,6 +2,9 @@ import { DefaultTokens } from "@openfun/cunningham-tokens";
 
 export const tokens = (defaults: DefaultTokens) => ({
   width: "292px",
-  "font-size": defaults.theme.font.sizes.s,
-  color: defaults.theme.colors["greyscale-600"],
+  "font-size": defaults.globals.font.sizes.s,
+  color: defaults.contextuals.content.semantic.neutral.secondary,
+  "color--error": defaults.contextuals.content.semantic.error.secondary,
+  "color--success": defaults.contextuals.content.semantic.success.secondary,
+  "color--disabled": defaults.contextuals.content.semantic.disabled.primary,
 });
