4185f8aad147fddb4cbd938fb21a1f7b62ebfd57
Nathan Panchout <nathanpanchout@hotmail.com>
2026-03-23 16:50:39 +0100

    wip

--- packages/react/src/components/Forms/DatePicker/tokens.ts
+++ packages/react/src/components/Forms/DatePicker/tokens.ts
@@ -18,21 +18,6 @@ export const tokens = (defaults: DefaultTokens) => ({
     defaults.contextuals.content.semantic.disabled.primary,
   "font-size": defaults.globals.font.sizes.sm,
   height: "3.5rem",
-  "item-background-color--hover":
-    defaults.contextuals.background.semantic.neutral["tertiary-hover"],
-  "item-background-color--selected":
-    defaults.contextuals.background.semantic.brand.tertiary,
-  "item-color": defaults.contextuals.content.semantic.neutral.primary,
-  "item-font-size": defaults.globals.font.sizes.sm,
   "background-color": defaults.contextuals.background.surface.primary,
-  "menu-background-color": defaults.contextuals.background.surface.primary,
-  "range-selection-background-color":
-    defaults.contextuals.background.semantic.brand.tertiary,
-  "range-selection-background-color--disabled":
-    defaults.contextuals.background.semantic.neutral.tertiary,
-  "grid-cell--border-color--today":
-    defaults.contextuals.border.semantic.brand.primary,
-  "grid-cell--color--today":
-    defaults.contextuals.content.semantic.brand.primary,
   "label-color--focus": defaults.contextuals.content.semantic.brand.primary,
 });
